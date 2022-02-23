e6400f286981e9ede627558d69a1d70524f49490
rafaelmarquezani <64435424+rafaelmarquezani@users.noreply.github.com>
2022-02-23 20:25:35 -0300

    Aula 23/02

---
+++ Aula2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Hello, world!</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+<h1>Olá! Hoje é dia!
+    </?php echo date ('d/m/y'); ?>
+</h1>  
+
+</body>
+</html>
