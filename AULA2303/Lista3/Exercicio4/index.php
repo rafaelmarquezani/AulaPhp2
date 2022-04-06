5f1d460853c7a0cf0f222e541effb2f6d35d7692
rafaelmarquezani <64435424+rafaelmarquezani@users.noreply.github.com>
2022-04-06 18:33:02 -0300

    Att

--- AULA2303/Lista3/Exercicio4/index.php
+++ AULA2303/Lista3/Exercicio4/index.php
@@ -8,15 +8,15 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Exercicio1</title>
+    <title>Exercicio4</title>
   </head>
   <body  class="container">
-    <h1>Ezxercicio 1!</h1>
+    <h1>Ezxercicio 4!</h1>
 
     <form action="resposta.php" method="post">
     <div class="row">
         <?php
-        for($i=1; $i<=10; $i++){
+        for($i=1; $i<=5; $i++){
 
         ?>
     <div class= "col">
